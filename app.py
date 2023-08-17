2a0ff3755bea7e186206e1d787e92149017d641e
RenatoDourad0 <renatocvdourado@hotmail.com>
2023-08-17 15:28:11 -0300

    update ghactions

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
